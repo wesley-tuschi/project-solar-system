ef91b99959225b62ae2d327b341a3748b81c20ab
Wesley Tuschi <levituschi@gmail.com>
2023-03-01 17:18:24 -0300

    Exercicio 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planetsItem) => (
+          <PlanetCard
+            key={ planetsItem.id }
+            planetName={ planetsItem.name }
+            planetImage={ planetsItem.image }
+          />
+        ))}
       </div>
+
     );
   }
 }
